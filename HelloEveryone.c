1d1f83d809c58d99770ebebeb1abfb4fd3569e9e
itsaparty <itsaparty@hanmail.net>
2017-03-10 00:08:11 +0900

    /을 \로 바꿈

--- HelloEveryone.c
+++ HelloEveryone.c
@@ -2,15 +2,15 @@
 int a, b;
 int main()
 {
-	printf("Hello Everyone!/n");
-	printf("Now Start!!/n");
-	printf("Make branch/n");
-	printf("Great!!/n");
-	printf("Go on!!!!/n");
-	printf("danger/n");
-	printf("Bomb/n");
-	printf("what???/n");
-	printf("add develop3/n");
-	printf("why??/n");
+	printf("Hello Everyone!\n");
+	printf("Now Start!!\n");
+	printf("Make branch\n");
+	printf("Great!!\n");
+	printf("Go on!!!!\n");
+	printf("danger\n");
+	printf("Bomb\n");
+	printf("what???\n");
+	printf("add develop3\n");
+	printf("why??\n");
 	return 0;
 }
